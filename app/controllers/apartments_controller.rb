ee07af1b25a871b1ebe3e1ab7aff54dd440c4a72
John <johnfarris217@gmail.com>
2023-10-12 19:17:40 -0500

    commit tree 5110ca2132cfb33a6fc5aad7680795a97cae4be7

---
+++ app/controllers/apartments_controller.rb
@@ -0,0 +1,45 @@
+class ApartmentsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        apartments = Apartment.all
+        render json: apartments, only: [:id, :number]
+    end
+
+    def show
+        apartment = Apartment.find(params[:id])
+        render json: apartment
+    end
+
+    def create
+        apartment = Apartment.create!(apartment_params)
+        render json: apartment, status: :created
+    end
+
+    def update
+        apartment = Apartment.find(params[:id])
+        apartment.update!(apartment_params)
+        render json: apartment
+    end
+
+    def destroy
+        apartment = Apartment.find(id: params[:id])
+        apartment.destroy
+        head :no_content
+    end
+
+    private
+
+    def apartment_params
+        params.permit(:number)
+    end
+
+    def render_not_found_response
+        render json: { error: "Apartment not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :leases, only: [:create, :destroy]
+  resources :tenants, only: [:create, :show, :update, :destroy, :index]
+  resources :apartments, only: [:create, :show, :update, :destroy, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
